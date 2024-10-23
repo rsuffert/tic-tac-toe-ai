e1b19b509b297c960ae8e0cfbd9af15dae8afeb7
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-10-23 17:52:35 -0300

    add initial implementation for minimax player

--- aiplayers.py
+++ aiplayers.py
@@ -2,7 +2,7 @@
 
 from abc import ABC, abstractmethod
 from enum import Enum
-from typing import List
+from typing import List, Tuple
 import random
 
 class Player(Enum):
@@ -23,13 +23,23 @@ class Difficulty(Enum):
 class BaseAIPlayer(ABC):
     '''Abstract class for an AI player.'''
     @abstractmethod
-    def play(difficulty: Difficulty, board: List[List[Player]]) -> List[List[Player]]: pass
+    def play(self, difficulty: Difficulty, board: List[List[Player]]) -> List[List[Player]]: pass
 
 class RandomAIPlayer(BaseAIPlayer):
     '''AI player that makes random moves.'''
-    def play(difficulty: Difficulty, board: List[List[Player]]) -> List[List[Player]]:
+    def play(self, difficulty: Difficulty, board: List[List[Player]]) -> List[List[Player]]:
         empty_cells = [(r, c) for r in range(3) for c in range(3) if board[r][c] == Player.EMPTY]
         if empty_cells:
             r, c = random.choice(empty_cells)
             board[r][c] = Player.O
         return board
+
+class MinimaxAIPlayer(BaseAIPlayer):
+    '''AI player that uses the minimax algorithm to make moves.'''
+    def play(self, difficulty: Difficulty, board: List[List[Player]]) -> List[List[Player]]:
+        rnd: int = random.randint(1, 10)
+        if rnd <= difficulty.value * 10:
+            return RandomAIPlayer().play(difficulty, board)
+        
+        # TODO: Replace random play with Minimax play here
+        return RandomAIPlayer.play(difficulty, board)
