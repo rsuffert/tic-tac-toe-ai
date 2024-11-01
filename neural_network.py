6db815dac39377b5aa713bd379550c58a417611c
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-11-01 00:20:01 -0300

    make logistic func the default activation func for neuron

--- neural_network.py
+++ neural_network.py
@@ -5,6 +5,12 @@ from math import exp
 
 ActivationFunc = Callable[[float], float]
 
+def logistic_func(x: float) -> float:
+    """
+    Logistic activation function.
+    """
+    return 1.0 / (1.0 + exp(-x))
+
 class Neuron:
     """
     Represents a neuron in a neural network.
@@ -15,7 +21,7 @@ class Neuron:
         """
         self.weights = weights
     
-    def propagate(self, inputs: List[float], f: ActivationFunc) -> float:
+    def propagate(self, inputs: List[float], f: ActivationFunc = logistic_func) -> float:
         """
         Propagates the inputs through the neuron and returns the output.
         """
@@ -23,9 +29,3 @@ class Neuron:
             raise ValueError("The number of inputs must be equal to the number of weights minus 1 (the bias).")
         inputs = [1] + inputs # add a neutral weight for the bias for easy computation
         return f(sum([i * w for i, w in zip(inputs, self.weights)]))
-    
-def logistic_func(x: float) -> float:
-    """
-    Logistic activation function.
-    """
-    return 1.0 / (1.0 + exp(-x))
