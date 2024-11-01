4135b841761efe8bd5c094c0a06770d319fbd5d3
Ricardo Süffert <ricardo.suffert@edu.pucrs.br>
2024-11-01 00:12:15 -0300

    add neuron class

---
+++ neural_network.py
@@ -0,0 +1,31 @@
+# neural_network
+
+from typing import List, Callable
+from math import exp
+
+ActivationFunc = Callable[[float], float]
+
+class Neuron:
+    """
+    Represents a neuron in a neural network.
+    """
+    def __init__(self, weights: List[float]) -> None:
+        """
+        Initializes the neuron instance with the given weights.
+        """
+        self.weights = weights
+    
+    def propagate(self, inputs: List[float], f: ActivationFunc) -> float:
+        """
+        Propagates the inputs through the neuron and returns the output.
+        """
+        if len(inputs) != len(self.weights) - 1:
+            raise ValueError("The number of inputs must be equal to the number of weights minus 1 (the bias).")
+        inputs = [1] + inputs # add a neutral weight for the bias for easy computation
+        return f(sum([i * w for i, w in zip(inputs, self.weights)]))
+    
+def logistic_func(x: float) -> float:
+    """
+    Logistic activation function.
+    """
+    return 1.0 / (1.0 + exp(-x))
