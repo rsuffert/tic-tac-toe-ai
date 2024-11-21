3add1c6bebe1598231a588b090546160071eec35
Guilherme Moraes <guimps1909@gmail.com>
2024-11-21 00:30:51 -0300

    update

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,8 +1,13 @@
 import random
 from typing import List, Tuple
 from population import Population
-from neural_network import NeuralNetwork
-from aiplayers import Difficulty
+from aiplayers import Difficulty, MinimaxAIPlayer, Player
+from tictactoeutils import Classifier
+
+classifier: Classifier = Classifier(
+    Player.X.value, Player.O.value, Player.EMPTY.value)
+
+minimax: MinimaxAIPlayer = MinimaxAIPlayer()
 
 
 class GeneticAlgorithm:
@@ -16,61 +21,79 @@ class GeneticAlgorithm:
         self.population = Population(population_size, topology)
 
     def run(self):
-        generation = 1
-        while generation <= self.max_generations:
-            self.population.updateFitness(Difficulty.EASY)
-            self.population.sortIndividualsByFitness()
-            best_fitness = self.population.getIndividual(0)[-1]
-            print(f"Generation {generation}: Best fitness = {best_fitness}")
-            self.population = self.new_generation(self.population)
-            generation += 1
+        print("Running genetic algorithm...")
+        difficulty = self.random_difficulty()
+        self.population.updateFitness(difficulty, self.elitism)
+        self.population.sortIndividualsByFitness()
+        best_fitness = self.population.getIndividual(0)[-1]
+        print(f"Generation 1: Best fitness = {
+            best_fitness}, Difficulty = {difficulty.name}")
 
-    def new_generation(self, population: Population) -> Population:
+        for generation in range(2, self.max_generations + 1):
+            difficulty = self.random_difficulty()
+            self.population = self.new_generation(self.population, difficulty)
+            best_fitness = self.population.getIndividual(0)[-1]
+            print(f"Generation {generation}: Best fitness = {
+                best_fitness}, Difficulty = {difficulty.name}")
+
+    def new_generation(self, population: Population, difficulty: Difficulty) -> Population:
         new_population = Population(self.population_size, self.topology)
+        new_population.clear()
         if self.elitism:
-            new_population.setIndividual(0, population.getIndividual(0))
+            new_population.individuals.append(population.getIndividual(0))
 
         while len(new_population.individuals) < self.population_size:
-            parents = self.selection_tournament(population)
-            if random.random() <= self.crossover_rate:
-                offspring = self.crossover(parents[0], parents[1])
-            else:
-                offspring = parents
+            parent1 = self.selection_tournament(population)
+            parent2 = self.selection_tournament(population)
+            offspring = self.crossover(parent1, parent2)
 
             for child in offspring:
                 self.mutate(child)
                 new_population.individuals.append(child)
 
-        new_population.updateFitness(Difficulty.EASY)
+        new_population.updateFitness(difficulty, self.elitism)
         new_population.sortIndividualsByFitness()
         return new_population
 
-    def selection_tournament(self, population: Population) -> List[List[float]]:
+    def selection_tournament(self, population: Population) -> List[float]:
         tournament_size = 2
         selected = random.sample(population.individuals, tournament_size)
         selected.sort(key=lambda ind: ind[-1], reverse=True)
-        return selected[:2]
+        return selected[0]
 
     def crossover(self, parent1: List[float], parent2: List[float]) -> List[List[float]]:
-        crossover_point = random.randint(0, len(parent1) - 1)
-        child1_weights = parent1[:crossover_point] + \
-            parent2[crossover_point:-1] + [0.0]
-        child2_weights = parent2[:crossover_point] + \
-            parent1[crossover_point:-1] + [0.0]
-        return [child1_weights, child2_weights]
+        child_weights = [(w1 + w2) / 2 for w1,
+                         w2 in zip(parent1[:-1], parent2[:-1])]
+        child_weights.append(0.0)  # Initialize fitness value for the child
+        return [child_weights]
 
     def mutate(self, individual: List[float]):
         for i in range(len(individual) - 1):
             if random.random() <= self.mutation_rate:
                 individual[i] = random.uniform(-1.0, 1.0)
 
+    def random_difficulty(self) -> Difficulty:
+        """
+        Generates a random difficulty with the following probabilities:
+        - 50% chance of being EASY
+        - 25% chance of being MEDIUM
+        - 25% chance of being HARD
+        """
+        rand = random.random()
+        if rand < 0.3:
+            return Difficulty.EASY
+        elif rand < 0.66:
+            return Difficulty.MEDIUM
+        else:
+            return Difficulty.HARD
+
 
 if __name__ == "__main__":
-    population_size = 100
+    population_size = 15
     topology = (9, 9, 9)
-    crossover_rate = 0.7
+    crossover_rate = 0.9
     mutation_rate = 0.01
-    max_generations = 1000
+    max_generations = 15
     elitism = True
 
     ga = GeneticAlgorithm(population_size, topology,
